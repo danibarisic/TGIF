5c50ad389eb251aeec9d189e3ba13dea1e459694
Dani Barisic <barisicdani@gmail.com>
2025-03-05 12:33:43 +0100

    minor fixes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-export const fetchJson = ('./HouseData.json').then((data) => {
-    document.querySelector('house-table-body').innerHTML = makeMemberRows(data);
-});
+export const fetchJsonHouse = async () => {
+    const response = await fetch('./HouseData.json');
+    const data = await response.json();
+    document.querySelector('.house-table-body').innerHTML = makeMemberRows(data);
+};
 
 export const makeMemberRows = (data) => {
     const tableBody = document.querySelector('house-table-body');
@@ -17,26 +18,4 @@ export const makeMemberRows = (data) => {
     });
 }
 
-fetchJson();
-=======
-export const fetchJson = ('./HouseData.json').then((data) => {
-    document.querySelector('house-table-body').innerHTML = makeMemberRows(data);
-});
-
-export const makeMemberRows = (data) => {
-    const tableBody = document.querySelector('house-table-body');
-    data.forEach(item => {
-        const row = document.createElement("tr");
-
-        Object.values(item).forEach(value => {
-            const cell = document.createElement("td");
-            cell.textContent = value;
-            row.appendChild(cell);
-        });
-        tableBody.appendChild(row);
-    });
-}
-
-fetchJson();
->>>>>>> 24a46fce439760c181de07104d24a8ea87aa7298
-makeMemberRows();
+fetchJsonHouse();
