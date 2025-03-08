2ba7549d748144a6d5bec54b06b77629781d4d53
Dani Barisic <barisicdani@gmail.com>
2025-03-08 14:29:11 +0100

    Created function 'buildStateMenu.'

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ export const fetchJsonHouse = async () => {
             name: member.first_name + ' ' + member.last_name,
             state: member.state,
             party: member.party,
-            years: (parseInt(member.begin_date) - parseInt(member.end_date)) * -1,
-            percVotes: null
+            years: (parseInt(member.begin_date) - parseInt(member.end_date)) * -1
         }));
-
 
         if (Array.isArray(members)) {
             makeMemberRowsHouse(selectMember);
@@ -18,7 +16,7 @@ export const fetchJsonHouse = async () => {
             console.error('No Members.');
         }
     } catch (error) {
-        console.error('Error');
+        console.error(error);
     }
 };
 
@@ -31,47 +29,120 @@ export const fetchJsonSenate = async () => {
             name: member.first_name + ' ' + member.last_name,
             state: member.state,
             party: member.party,
-            years: null,
+            years: member.seniority,
             percVotes: member.votes_with_party_pct
         }));
-
         if (Array.isArray(members)) {
             makeMemberRowsSenate(selectMember);
         } else {
             console.error('No members found.');
         }
     } catch (error) {
-        console.error('Error');
+        console.error(error);
     }
 };
 
-export const makeMemberRowsHouse = (data) => {
+export const createAnchorsHouse = (senatorData) => {
+    const senatorNameCells = document.querySelectorAll('.house-senator-name');
+
+    senatorNameCells.forEach(cell => {
+        const senatorName = cell.textContent.trim();
+        const senatorInfo = senatorData.find(senator => senator.Name === senatorName);
+
+        const link = document.createElement('a');
+        link.textContent = senatorName;
+        link.href = senatorInfo.api_uri;
+        cell.textContent = '';
+        cell.appendChild(link);
+    })
+};
+
+export const makeMemberRowsHouse = (data, partyFilter, stateFilter) => {
     const tableBody = document.querySelector('#house-table-body');
+
     data.forEach((item, index) => {
         const row = document.createElement("tr");
+        console.log(item);
 
-        Object.values(item).forEach((value, valueIndex) => {
-            const cell = document.createElement("td");
+        Object.entries(item).forEach(([key, value], valueIndex) => {
+            const cell = document.createElement('td');
+            if (key === 'name') {
+                cell.classList.add('house-senator-name');
+            }
             cell.textContent = value;
             row.appendChild(cell);
         });
         tableBody.appendChild(row);
     });
+    createAnchorsHouse();
 };
 
-export const makeMemberRowsSenate = (data) => {
+export const createAnchorsSenate = (senatorData) => {
+    const senatorNameCells = document.querySelectorAll('.senate-senator-name');
+
+    senatorNameCells.forEach(cell => {
+        const senatorName = cell.textContent.trim();
+        const senatorInfo = senatorData.find(senator => senator.name === senatorName);
+
+        const link = document.createElement('a');
+        link.textContent = senatorName;
+        link.href = senatorInfo.url;
+        cell.textContent = '';
+        cell.appendChild(link);
+    })
+};
+
+export const makeMemberRowsSenate = (data, partyFilter, stateFilter) => {
     const tableBody = document.querySelector('#senate-table-body');
-    data.forEach((item, index) => {
+    const filteredData = data.filter(item => {
+        const partyMatch = !partyFilter || item.party === partyFilter;
+        const stateMatch = !stateFilter || item.state === stateFilter;
+        return partyMatch && stateMatch;
+    })
+    filteredData.forEach((item, index) => {
         const row = document.createElement("tr");
 
-        Object.values(item).forEach((value, valueIndex) => {
+        Object.entries(item).forEach(([key, value], valueIndex) => {
             const cell = document.createElement("td");
+            if (key === 'name') {
+                cell.classList.add('senate-senator-name')
+            }
+            const link = document.createElement('a');
+            link.textContent = value;
+            cell.innerHTML = '';
             cell.textContent = value;
             row.appendChild(cell);
         });
         tableBody.appendChild(row);
     });
+    createAnchorsSenate(filteredData);
 };
 
+export const buildStateMenu = async () => {
+    try {
+        const response = await fetch('states.json');
+        const states = await response.json();
+        const selectElement = document.querySelector('#select-state');
+        const defaultOption = document.createElement('option');
+
+        defaultOption.value = '';
+        defaultOption.textContent = 'Select a State:';
+        selectElement.appendChild(defaultOption);
+
+        for (const abbreviation in states) {
+            if (states.hasOwnProperty(abbreviation)) {
+                const stateName = states[abbreviation];
+                const option = document.createElement('option');
+                option.value = abbreviation;
+                option.textContent = stateName;
+                selectElement.appendChild(option);
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching');
+    }
+}
+
+buildStateMenu();
 fetchJsonHouse();
 fetchJsonSenate();
